a64156206216132eaa7d146b9456ae1ac79ba739
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-18 02:17:41 +0100

    more fixes

--- api/login.php
+++ api/login.php
@@ -3,11 +3,12 @@
 // required headers
 header("Access-Control-Allow-Origin: *");
 // header("Content-Type: application/json; charset=UTF-8");
+
 header('Content-Type: application/json');
-header("Access-Control-Allow-Methods: POST");
-header('Access-Control-Allow-Methods: GET, POST');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
-header("Access-Control-Max-Age: 3600");
+// header("Access-Control-Allow-Methods: POST");
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header("Access-Control-Max-Age: 86400");
 // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 // header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
