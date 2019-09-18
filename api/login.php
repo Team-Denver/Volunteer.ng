bcd907c852f9601f09bf343f598fb3827991b9f0
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-18 02:14:41 +0100

    added new headers

--- api/login.php
+++ api/login.php
@@ -5,9 +5,11 @@ header("Access-Control-Allow-Origin: *");
 // header("Content-Type: application/json; charset=UTF-8");
 header('Content-Type: application/json');
 header("Access-Control-Allow-Methods: POST");
+header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 header("Access-Control-Max-Age: 3600");
 // header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization,X-Requested-With');
 
 // files needed to connect to database
 include_once '../config/database.php';
