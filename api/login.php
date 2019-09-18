fbf1aa96146d724ed70bac2be3fb81ea0b34ef8c
Jude Jonathan <jonathanjude27@gmail.com>
2019-09-18 02:24:09 +0100

    chnages

--- api/login.php
+++ api/login.php
@@ -4,7 +4,7 @@
 header("Access-Control-Allow-Origin: *");
 // header("Content-Type: application/json; charset=UTF-8");
 
-header('Content-Type: application/json');
+// header('Content-Type: application/json');
 // header("Access-Control-Allow-Methods: POST");
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
